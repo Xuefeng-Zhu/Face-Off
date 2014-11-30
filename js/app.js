a3be7835d3b685d495bb5959a77fbf82c44d1acb
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-11-30 00:33:31 -0600

    finish basic layout

--- js/app.js
+++ js/app.js
@@ -4,14 +4,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
-  $routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegisterCtrl'});
-  $routeProvider.when('/lists/:status', {templateUrl: 'partials/lists.html', controller: 'ListsCtrl'});
+  $routeProvider.when('/menu', {templateUrl: 'partials/menu.html', controller: 'MenuCtrl'});
   $routeProvider.otherwise({redirectTo: '/login'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -3,126 +3,33 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-.controller('LoginCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
-    $scope.login = function() {
-        Todo.startSession({
-            email: $scope.email,
-            password: $scope.password,
-            success: function(user) {
-                $rootScope.user = user; 
-                $location.path('/lists/incomplete');
-                $scope.$apply();
-            },
-            error: function(xhr) {
-                alert(xhr.responseJSON.error);
+    .controller('LoginCtrl', ['$scope', '$rootScope', '$location',
+        function($scope, $rootScope, $location) {
+            $scope.login = function() {
+                FB.login(function(response) {
+                    $rootScope.facebook = response;
+                    $location.path('/menu');
+                    $rootScope.$apply();
+                });
             }
-        });
-    }
 
-}])
-.controller('RegisterCtrl', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
-    $scope.register = function() {
-    	if ($scope.password1 != $scope.password2){
-    		alert('Passwords do not match');
-    		return;
-    	}
-
-        Todo.createUser({
-            email: $scope.email,
-            password: $scope.password1,
-            success: function(user) {
-                $rootScope.user = user; 
-                $location.path('/lists/incomplete');
-                $scope.$apply();
-            },
-            error: function(xhr) {
-               alert([$scope.email,xhr.responseJSON.email[0]].join(' '));
-            }
-        });
-    }
-}])
-.controller('ListsCtrl', ['$scope','$rootScope', '$location', '$routeParams', function($scope, $rootScope, $location, $routeParams){
-    if (!$rootScope.user){
-        alert("Login fails");
-        $location.path('/login');
-        return;
-    }
-
-    if ($routeParams['status'] == 'complete'){
-       $scope.status = true; 
-    }
-    else{
-        $scope.status = false;
-    }
-    
-    Todo.loadTodos({
-        success: function(todos) {
-            $scope.todos = [];
-            for (var i in todos){
-                if (todos[i].is_complete == $scope.status){
-                    $scope.todos.push(todos[i]);
-                }
-            }
-            $scope.$apply();
-        },
-        error: function(xhr) {
-            alert("Fail to load list");
         }
-    });
-
-	$scope.addTodo = function(){
-        Todo.createTodo({
-            todo: {description: $scope.newTodo},
-            success: function(todo){
-                $scope.todos.push(todo);
-                $scope.newTodo = null;
-                $scope.$apply();
-            },
-            error: function(xhr) {
-                alert("An error occurs when you are trying to add a todo");
+    ])
+    .controller('MenuCtrl', ['$scope', '$rootScope', '$location', '$routeParams',
+        function($scope, $rootScope, $location, $routeParams) {
+            if (!$rootScope.facebook) {
+                alert("Login fails");
+                $location.path('/login');
+                return;
             }
-        })
-    }
 
-    $scope.toggleTodo = function(id) {
-        setTimeout(function() {
-            Todo.updateTodo({
-                todoId: $scope.todos[id].id,
-                data: {
-                    is_complete: !$scope.todos[id].is_complete
-                },
-                success: function() {
-                    $scope.todos[id].is_complete = !$scope.todos[id].is_complete;
-                    $scope.$apply();
-                },
-                error: function(xhr) {
-                    alert('todo update error!')
-                }
-            });
-        }, 100);
-    }
+            console.log($rootScope.facebook);
+            $scope.sendMessage = function() {
+                FB.ui({
+                    method: 'send',
+                    link: "http://xuefeng-zhu.github.io/Song-Gradient/app/#/main"
+                });
+            }
 
-    $scope.editTodo = function(id) {
-        $scope.editing = {
-            text: $scope.todos[id].description,
-            id: id
-        };
-    }
-
-    $scope.doneEdit = function(){
-         Todo.updateTodo({
-                todoId: $scope.todos[$scope.editing.id].id,
-                data: {
-                    description: $scope.editing.text
-                },
-                success: function() {
-                    $scope.todos[$scope.editing.id].description = $scope.editing.text;
-                    $scope.editing = null;
-                    $scope.$apply();
-                },
-                error: function(xhr) {
-                    alert('todo update error!')
-                }
-            });
-    }
-}]);
+        }
+    ]);
